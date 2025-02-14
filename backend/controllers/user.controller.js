ec6f36242716cc99e780206a5075ea0e98220bd9
Manjunath182004 <its.appu18@gmail.com>
2025-02-14 14:47:06 +0530

    4th part completed-socket

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,4 +14,4 @@ export const getUsersForSidebar = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
 
     }
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+//import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,24 +8,26 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import Message from "./models/message.model.js";
-
-const app = express();
-
-const PORT = process.env.PORT || 5000;
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-app.use(express.json()); // to parse the incoming request with JSON payloads (req.body)
+//const __dirname = path.resolve();
+ 
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
+//app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
+//app.get("*", (req, res) => {
+//	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+//});
 
 
 //app.get("/", (req, res) => {
@@ -35,8 +37,7 @@ app.use("/api/users",userRoutes);
 //});
 
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log('server running on port ${PORT}');
 });
